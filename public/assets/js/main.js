80d46eb54288e10fde909a04b929e84e1b6a7b34
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2018-02-27 01:31:34 +0200

    New Release

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -26,20 +26,20 @@ jQuery(document).ready(function ($) {
 		}, 1500, 'easeInOutExpo');
 		event.preventDefault();
 	});
-	
-	
+
+
 	jQuery(window).scroll(function () {
 	  var top = jQuery(document).scrollTop();
 		var height = 300;
 	  //alert(batas);
-	  
+
 	  if (top > height) {
 		jQuery('.navbar-fixed-top').addClass('menu-scroll');
 	  } else {
 	   jQuery('.navbar-fixed-top').removeClass('menu-scroll');
 	  }
-	});	
- // Scroll up 
+	});
+ // Scroll up
 
     $(window).scroll(function(){
         if ($(this).scrollTop() > 600) {
@@ -51,8 +51,39 @@ jQuery(document).ready(function ($) {
     $('.scrollup').click(function(){
         $("html, body").animate({ scrollTop: 0 }, 1000);
         return false;
-    }); 
- 
+    });
+
  new WOW().init();
 
 });
+
+
+
+
+// ================================== 	Alert box =========================
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
